aa8edbc5ad8ebd404748bb70a3b2bbda892fd8be
Neil MacGregor <neil.macgregor@ualberta.ca>
2020-11-29 14:42:28 -0700

    Experimenting with building in support for a JSON data struct

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+import json
+
 app = FastAPI()
 
 
@@ -23,7 +25,9 @@ def read_item(item_id: int, q: Optional[str] = None):
 
 @app.get("/description/{hostname}")
 def read_item(hostname: str, q: Optional[str] = None):
-    return {"hostname": hostname, "q": q}
+    data = '{"name": "veronica", "description": "laptop" }'
+    data_dict = json.loads(data)
+    return {data_dict["name"]: data_dict["description"], "q": q}
 
 
 @app.put("/items/{item_id}")
